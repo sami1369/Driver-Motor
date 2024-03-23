0aba7a61df573a1e4b2516f7da453d41d7def22a
sami1369 <samisaeid1990@gmail.com>
2024-03-23 11:59:50 +0100

    add wifi lib and test it

--- sample_project/main/main.cpp
+++ sample_project/main/main.cpp
@@ -1,19 +1,56 @@
 
-#include "gpio.h"
+#include <string>
+#include <iostream>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "nvs_flash.h"
 
+#include "wifi.h"
 
-
-
+Wifi::state_e wifiState{Wifi::state_e::NOT_INITIALIZED};
+Wifi Wifi;
 extern "C" void app_main(void)
 {
-    Gpio *led1, *key1; 
-    led1 = new Gpio{GPIO_NUM_2, false, GPIO_MODE_OUTPUT};
-    key1= new Gpio{GPIO_NUM_15, false, GPIO_MODE_INPUT};
-    
+    esp_event_loop_create_default();
+    nvs_flash_init();
+
+    Wifi.SetCredentials("TP-Link_8020", "50824823");
+    Wifi.Init();
     while (true)
     {
-        led1->setLevel(key1->read());
+        vTaskDelay(pdMS_TO_TICKS(10000));
+
+        wifiState = Wifi.GetState();
+        switch (wifiState)
+        {
+        case Wifi::state_e::READY_TO_CONNECT:
+            std::cout << "Wifi Status: READY_TO_CONNECT\n";
+            Wifi.Begin();
+            break;
+        case Wifi::state_e::DISCONNECTED:
+            std::cout << "Wifi Status: DISCONNECTED\n";
+            Wifi.Begin();
+            break;
+        case Wifi::state_e::CONNECTING:
+            std::cout << "Wifi Status: CONNECTING\n";
+            break;
+        case Wifi::state_e::WAITING_FOR_IP:
+            std::cout << "Wifi Status: WAITING_FOR_IP\n";
+            break;
+        case Wifi::state_e::ERROR:
+            std::cout << "Wifi Status: ERROR\n";
+            break;
+        case Wifi::state_e::CONNECTED:
+            std::cout << "Wifi Status: CONNECTED\n";
+            break;
+        case Wifi::state_e::NOT_INITIALIZED:
+            std::cout << "Wifi Status: NOT_INITIALIZED\n";
+            break;
+        case Wifi::state_e::INITIALIZED:
+            std::cout << "Wifi Status: INITIALIZED\n";
+            break;
+        }
     }
 }
+

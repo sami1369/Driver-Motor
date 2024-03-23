0bfa9a5682f00a5e4eb702e442a0d0c399e5081a
sami1369 <samisaeid1990@gmail.com>
2024-03-23 12:17:43 +0100

    create class in main.h

--- sample_project/main/main.cpp
+++ sample_project/main/main.cpp
@@ -1,56 +1,14 @@
+#include "main.h"
 
-#include <string>
-#include <iostream>
+Main App;
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "nvs_flash.h"
-
-#include "wifi.h"
-
-Wifi::state_e wifiState{Wifi::state_e::NOT_INITIALIZED};
-Wifi Wifi;
 extern "C" void app_main(void)
 {
-    esp_event_loop_create_default();
-    nvs_flash_init();
+    App.setup();
 
-    Wifi.SetCredentials("TP-Link_8020", "50824823");
-    Wifi.Init();
     while (true)
     {
         vTaskDelay(pdMS_TO_TICKS(10000));
-
-        wifiState = Wifi.GetState();
-        switch (wifiState)
-        {
-        case Wifi::state_e::READY_TO_CONNECT:
-            std::cout << "Wifi Status: READY_TO_CONNECT\n";
-            Wifi.Begin();
-            break;
-        case Wifi::state_e::DISCONNECTED:
-            std::cout << "Wifi Status: DISCONNECTED\n";
-            Wifi.Begin();
-            break;
-        case Wifi::state_e::CONNECTING:
-            std::cout << "Wifi Status: CONNECTING\n";
-            break;
-        case Wifi::state_e::WAITING_FOR_IP:
-            std::cout << "Wifi Status: WAITING_FOR_IP\n";
-            break;
-        case Wifi::state_e::ERROR:
-            std::cout << "Wifi Status: ERROR\n";
-            break;
-        case Wifi::state_e::CONNECTED:
-            std::cout << "Wifi Status: CONNECTED\n";
-            break;
-        case Wifi::state_e::NOT_INITIALIZED:
-            std::cout << "Wifi Status: NOT_INITIALIZED\n";
-            break;
-        case Wifi::state_e::INITIALIZED:
-            std::cout << "Wifi Status: INITIALIZED\n";
-            break;
-        }
+        App.run();
     }
 }
-
--- sample_project/main/main.h
+++ sample_project/main/main.h
@@ -1,8 +1,64 @@
+
 #pragma once
+#include <string>
+#include <iostream>
 
-// Main class used for testing only
-class Main 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+
+#include "wifi.h"
+
+class Main final
 {
+private:
 public:
+    Wifi::state_e wifiState { Wifi::state_e::NOT_INITIALIZED };
+    Wifi wifiApp;
+    void run(void);
+    void setup(void);
+};
 
-}; // Main Class
+void Main::run(void)
+{
+    wifiState = wifiApp.GetState();
+
+    switch (wifiState)
+    {
+    case Wifi::state_e::READY_TO_CONNECT:
+        std::cout << "Wifi Status: READY_TO_CONNECT\n";
+        wifiApp.Begin();
+        break;
+    case Wifi::state_e::DISCONNECTED:
+        std::cout << "Wifi Status: DISCONNECTED\n";
+        wifiApp.Begin();
+        break;
+    case Wifi::state_e::CONNECTING:
+        std::cout << "Wifi Status: CONNECTING\n";
+        break;
+    case Wifi::state_e::WAITING_FOR_IP:
+        std::cout << "Wifi Status: WAITING_FOR_IP\n";
+        break;
+    case Wifi::state_e::ERROR:
+        std::cout << "Wifi Status: ERROR\n";
+        break;
+    case Wifi::state_e::CONNECTED:
+        std::cout << "Wifi Status: CONNECTED\n";
+        break;
+    case Wifi::state_e::NOT_INITIALIZED:
+        std::cout << "Wifi Status: NOT_INITIALIZED\n";
+        break;
+    case Wifi::state_e::INITIALIZED:
+        std::cout << "Wifi Status: INITIALIZED\n";
+        break;
+    }
+}
+
+void Main::setup(void)
+{
+    esp_event_loop_create_default();
+    nvs_flash_init();
+
+    wifiApp.SetCredentials("TP-Link_8020", "50824823");
+    wifiApp.Init();
+}
